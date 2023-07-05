4bb2df46f9e2f6713e46928318b0e06f1fd5c275
indriyani13 <115499584+indriyani13@users.noreply.github.com>
2023-07-05 15:34:03 +0700

    main

--- DummyChatPlug.js
+++ DummyChatPlug.js
@@ -139,7 +139,6 @@ var $messages = $('.messages');
 var $areapesan = $(".chat-messages");
 var $areabalas = $("#AreaBalas");
 var connectionOptions = {
-  "reconnection": true,
   "force new connection": true,
   "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
   "timeout": 10000, //before connect_error and connect_timeout are emitted.
